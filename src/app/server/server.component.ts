b4d404b077b8ec2b49ea9ecf7159e3cc93a93573
antarus66 <antarus66@gmail.com>
2018-07-31 17:33:15 +0300

    Added properties for server component.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    id: number = 10;
+    status: string = 'offline';
 
+    getServerStatus() {
+        return this.status;
+    }
 }
