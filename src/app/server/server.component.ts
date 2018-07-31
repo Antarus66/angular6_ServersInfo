c64bb7df22e9131bd2d118d2e2e75f3e0e99243c
antarus66 <antarus66@gmail.com>
2018-07-31 14:05:31 +0300

    Added a server component and using it in the app component.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: "app-server",
-    templateUrl: "./server.component.html"
+    selector: 'app-server',
+    templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
